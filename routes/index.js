9d8a6cc96140e7de99437038544e90fcdc29424c
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2022-02-12 14:31:46 -0500

    url versionada optimizada

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
+const express = require('express');
+
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
 const basicRouter = require('./basic.router');
 
 function routerApi (app) {
-  app.use('/api/v1/products', productsRouter);
-  app.use('/api/v1/users', usersRouter);
-  app.use('/api/v1/categories', categoriesRouter);
-  app.use('/api/v1/', basicRouter);
+  const router = express.Router();
+  app.use("/api/v1", router);
+  router.use('/products', productsRouter);
+  router.use('/users', usersRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/', basicRouter);
 }
 
 module.exports = routerApi;
