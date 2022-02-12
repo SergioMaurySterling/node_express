c714e1d2e84df110bb673ceb5de53642dc30693e
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2022-02-12 16:24:45 -0500

    services of delete, find and create

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,21 +1,13 @@
 const express = require('express');
-const faker = require('faker');
-
+const ProductsService = require('./../services/product.service');
+// al ser una clase debo generar una instancia de esta
+const service = new ProductsService();
 // generar un router especifico para los productos
 const routerProducts = express.Router();
 
 // crear una ruta con parametros, si recibe un ?size=5, entonces devuelve 5 elementos
 routerProducts.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 20;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(),10),
-      image: faker.image.imageUrl(),
-    });
-  }
+  const products = service.find();
   res.json(products);
 })
 
@@ -25,40 +17,10 @@ routerProducts.get("/filter", (req, res) => {
   res.send('Yo soy un filter');
 })
 
-// crear una ruta dinamica, que recibe un id como parametro,
-// y devuelve un json con el objeto que tenga ese id
 routerProducts.get("/:id", (req, res) => {
-  let products = [
-    {
-      id: 1,
-      name: 'Product 1',
-      price: 1000
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      price: 2000
-    },
-    {
-      id: 3,
-      name: 'Product 3',
-      price: 3000
-    },
-    {
-      id: 4,
-      name: 'Product 4',
-      price: 4000
-    }
-  ]
   const { id } = req.params
-  const ans = products.find(product => product.id == id)
-  if (id == "999") {
-    res.status(404).json({
-      error: 'Product not found'
-    })
-  } else {
-    res.json([ans])
-  }
+  const ans = service.findOne(id);
+  res.json([ans])
 })
 
 
@@ -67,6 +29,7 @@ routerProducts.get("/:id", (req, res) => {
 routerProducts.post('/', (req, res) => {
   // la data se recibe en .body
   const body = req.body;
+  service.create(body);
   res.status(201).json({
     message: "Product created",
     data: body
@@ -75,9 +38,10 @@ routerProducts.post('/', (req, res) => {
 
 // Recibir un la modificacion parcial de un objeto
 routerProducts.patch('/:id', (req, res) => {
-  // la data se recibe en .body
+  // la data se recibe en body
   const { id } = req.params;
   const body = req.body;
+  service.update(id, body);
   res.json({
     message: "Product updated",
     data: body,
---
+++ services/product.service.js
@@ -0,0 +1,54 @@
+const faker = require('faker');
+
+// POO
+class ProductsService {
+
+  constructor(){
+    this.products = [];
+    this.generate();
+  }
+
+  generate(){
+    const limit = 10;
+    for (let index = 0; index < limit; index++) {
+      this.products.push({
+        id: faker.datatype.uuid(),
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(),10),
+        image: faker.image.imageUrl(),
+      });
+    }
+  }
+
+  create(body) {
+    const id = faker.datatype.uuid();
+    const { name, price, image } = body;
+    this.products.push({
+      id,
+      name,
+      price,
+      image,
+    });
+  }
+
+  find() {
+    return this.products;
+  }
+
+  findOne(id){
+    return this.products.find(product => product.id == id);
+  }
+
+  update(id,body) {
+    let indexOfProduct = this.products.findIndex(product => product.id == id);
+      this.products[indexOfProduct].name = body.name;
+      this.products[indexOfProduct].price = body.price;
+      this.products[indexOfProduct].image = body.image;
+  }
+
+  delete() {
+
+  }
+}
+
+module.exports = ProductsService;
