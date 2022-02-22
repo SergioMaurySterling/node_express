b559ca7199fdfd6584cbe1282f1c01fda0c187b7
SergioMaurySterling <52435269+SergioMaurySterling@users.noreply.github.com>
2022-02-22 16:50:17 -0500

    allow access to some domains

--- index.js
+++ index.js
@@ -16,8 +16,18 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
+const whitelist =['http://localhost:3000', 'http://localhost:3001', 'http://www.requi.com.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(options));
 routerApi(app);
-app.use(cors());
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
